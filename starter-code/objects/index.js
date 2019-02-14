15a539a93564a8d9e697aa72406371f68abf3579
Erin Trainor <trainor.erin@gmail.com>
2019-02-13 21:36:39 -0800

    car vehicle started with anthony

--- starter-code/objects/index.js
+++ starter-code/objects/index.js
@@ -1,14 +1,15 @@
 'use strict';
 
 
-const Vehicle = require('./constructor.js');
-// const Vehicle = require('./class.js');
+// const Vehicle = require('./vehicle-constructor.js');
+const Car = require('./car-class.js');
 // const Vehicle = require('./factory.js');
 
 // Implement a car and motorcycle
-const mazda = new Vehicle.Car('Mazda 3');
+const UseCarClass = new Car();
+const mazda = new Car('Mazda 3');
 console.log(mazda.name, mazda.drive(), mazda.stop());
 
-const harley = new Vehicle.Motorcycle('Harley');
-console.log(harley.name, harley.wheelie(), harley.stop());
+// const harley = new Vehicle.Motorcycle('Harley');
+// console.log(harley.name, harley.wheelie(), harley.stop());
 
