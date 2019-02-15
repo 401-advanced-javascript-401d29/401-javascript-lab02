08c8e14a5f4895e23236a5d7739f2b7312447dda
Erin Trainor <trainor.erin@gmail.com>
2019-02-14 18:42:44 -0800

    added parent and child classes for vehicles

---
+++ starter-code/objects/flying-vehicle-class.js
@@ -0,0 +1,39 @@
+'use strict';
+
+class flyingVehicle {
+    constructor(name, flight) {
+        this.name = name;
+        this.flight = flight;
+    }
+
+    flying() {
+        return 'a little bit of pixie dust';
+    }
+
+    crashing() {
+        return 'AHHHHHHHHHHHHHHHH!!!!!!!!! BOOM!!';
+    }
+}
+
+class Airplane extends flyingVehicle{
+    constructor(name) {
+        super(name, wings);
+    }
+}
+
+class Helicopter extends flyingVehicle {
+    constructor(name) {
+        super(name, rotors);
+    }
+
+    hover() {
+        return 'I am Floating!';
+    }
+}
+
+
+
+module.exports = {Airplane, Helicopter};
+
+
+
--- starter-code/objects/index.js
+++ starter-code/objects/index.js
@@ -2,16 +2,13 @@
 
 
 // const Vehicle = require('./vehicle-constructor.js');
-const Car = require('./car-class.js');
-const Motorcycle = require('./motorcycle-class.js');
+const Vehicle = require('./vehicle-class.js');
+const
 // const Vehicle = require('./factory.js');
 
 // Implement a car and motorcycle
-// const UseCarClass = new Car();
-// const UseMotorcycleClass = new Motorcycle();
-const mazda = new Car('Mazda 3');
+const mazda = new Vehicle.Car('Mazda 3');
 console.log(mazda.name, mazda.drive(), mazda.stop());
 
-const harley = new Motorcycle('Harley');
+const harley = new Vehicle.Motorcycle('Harley');
 console.log(harley.name, harley.wheelie(), harley.stop());
-
