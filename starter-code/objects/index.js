075b40d9f83d6e2d2b5432e6db6aade5e6e39f36
Erin Trainor <trainor.erin@gmail.com>
2019-02-14 16:46:06 -0800

    finished motorcycle

--- starter-code/objects/index.js
+++ starter-code/objects/index.js
@@ -3,13 +3,15 @@
 
 // const Vehicle = require('./vehicle-constructor.js');
 const Car = require('./car-class.js');
+const Motorcycle = require('./motorcycle-class.js');
 // const Vehicle = require('./factory.js');
 
 // Implement a car and motorcycle
-const UseCarClass = new Car();
+// const UseCarClass = new Car();
+// const UseMotorcycleClass = new Motorcycle();
 const mazda = new Car('Mazda 3');
 console.log(mazda.name, mazda.drive(), mazda.stop());
 
-// const harley = new Vehicle.Motorcycle('Harley');
-// console.log(harley.name, harley.wheelie(), harley.stop());
+const harley = new Motorcycle('Harley');
+console.log(harley.name, harley.wheelie(), harley.stop());
 
---
+++ starter-code/objects/motorcycle-class.js
@@ -0,0 +1,22 @@
+'use strict';
+
+class Motorcycle {
+    constructor(name, wheels) {
+        this.name = name;
+        this.wheels = wheels;
+    }
+
+    drive() {
+        return 'Moving Forward';
+    }
+
+    stop() {
+        return 'Stopping';
+    }
+    
+    wheelie() {
+        return 'Wheee!';
+    }
+}
+
+module.exports = Motorcycle;
